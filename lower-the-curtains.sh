6243e2374ab6d83bef54cbc61d9b0b43ee2f96c3
Jorres Tarasov <jorres.tarasov@gmail.com>
2020-12-28 02:44:31 +0300

    NEW: script for setting brightness below maximum

---
+++ lower-the-curtains.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Enter the number from 3 to 76, 20 will be used as a sensible default with no arguments"
+echo "Warning, 1-2 levels are making the screen completely black"
+
+BRIGHTNESS=$1
+
+if [ "$BRIGHTNESS" == "" ]; then
+    sudo su -c "echo $BRIGHTNESS >/sys/class/backlight/intel_backlight/brightness"
+fi
+
