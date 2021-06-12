547df621032635b043226d32eec5dc470081ea2e
Jorres Tarasov <jorres.tarasov@gmail.com>
2021-06-12 12:12:45 +0300

    NEW: include task and timew folders

--- apply-configs.sh
+++ apply-configs.sh
@@ -2,3 +2,5 @@
 cp -r nvim-config/* /home/jorres/.config/nvim/
 cp .zshrc /home/jorres/.zshrc
 cp .tmux.conf /home/jorres/.tmux.conf
+cp -r ./.task /home/jorres/.task 
+cp -r ./.timewarrior /home/jorres/.timewarrior 
--- update-configs.sh
+++ update-configs.sh
@@ -2,3 +2,5 @@
 cp -r /home/jorres/.config/nvim/* ./nvim-config
 cp /home/jorres/.zshrc ./.zshrc
 cp /home/jorres/.tmux.conf ./.tmux.conf
+cp -r /home/jorres/.task ./.task
+cp -r /home/jorres/.timewarrior ./.timewarrior
