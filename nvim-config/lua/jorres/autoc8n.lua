65cad98583fa0d5c8effff5a2a7459fc7800ab1a
Jorres <jorres.tarasov@gmail.com>
2021-07-23 15:00:01 +0300

    AUTO: data update 2021-07-23

--- nvim-config/lua/jorres/autoc8n.lua
+++ nvim-config/lua/jorres/autoc8n.lua
@@ -4,15 +4,15 @@ require'compe'.setup {
   enabled = true;
   autocomplete = true;
   debug = false;
-  min_length = 1;
-  preselect = 'enable';
+  min_length = 2;
+  preselect = 'always';
   throttle_time = 80;
   source_timeout = 200;
   resolve_timeout = 800;
   incomplete_delay = 400;
-  max_abbr_width = 1000;
-  max_kind_width = 1000;
-  max_menu_width = 1000;
+  max_abbr_width = 10;
+  max_kind_width = 10;
+  max_menu_width = 10;
 
   documentation = true;
 
