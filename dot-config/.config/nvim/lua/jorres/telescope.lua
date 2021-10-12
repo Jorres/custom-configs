f2ada71792b50349d1e827f1d7e308c440f788af
Jorres Tarasov <jorres.tarasov@gmail.com>
2021-10-12 11:32:23 +0300

    fix: plugged folder, temporary solution

--- dot-config/.config/nvim/lua/jorres/telescope.lua
+++ dot-config/.config/nvim/lua/jorres/telescope.lua
@@ -61,7 +61,6 @@ require('telescope').setup{
     }
 } 
 
-require("telescope").load_extension('fzy_native')
 require("telescope").load_extension('frecency')
 
 local M = {}
--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ stow -D dot-config
 stow -D tmux
 stow -D zsh
 
-stow dot-config
+stow --verbose=5 dot-config
 stow tmux
 stow zsh
