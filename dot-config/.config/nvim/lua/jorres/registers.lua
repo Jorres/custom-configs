821fc2f5a1c36a6b64569be5343d4ec1ac6f4626
Egor Tarasov <jorres.tarasov@gmail.com>
2022-02-25 02:17:09 +0300

    fix: registers config error

--- dot-config/.config/nvim/lua/jorres/registers.lua
+++ dot-config/.config/nvim/lua/jorres/registers.lua
@@ -1,9 +1,12 @@
 local config = require("nvim-peekup.config")
 
 -- for some reason the default is 700ms. Weird!
-config.on_keystroke["delay"] = "0ms"
+config.on_keystroke["delay"] = ""
 config.geometry["wrap"] = false
 
 -- Think of ways to recolor the window?)
+-- Make a PR to allow window options customization
+-- Make a PR to allow recoloring) 
+-- Good way to accumulate knowledge for your plugins
 
 SETMAP("n", "<leader>r", ":lua require('nvim-peekup').peekup_open()<CR>", {silent = true})
