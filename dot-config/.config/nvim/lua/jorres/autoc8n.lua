9ce7f8cc39ae2288d584ca2b5aa80e27b50cd3db
Egor Tarasov <jorres.tarasov@gmail.com>
2022-02-25 00:10:05 +0300

    chore(autoc8n): clean a bunch of old commends, heavily limit suggestions

--- dot-config/.config/nvim/lua/jorres/autoc8n.lua
+++ dot-config/.config/nvim/lua/jorres/autoc8n.lua
@@ -1,9 +1,9 @@
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
-local lspkind = require "lspkind"
+local lspkind = require"lspkind"
 lspkind.init()
 
-local cmp = require "cmp"
+local cmp = require"cmp"
 
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
@@ -14,20 +14,20 @@ local luasnip = require("luasnip")
 
 cmp.setup {
   mapping = {
+    -- ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    -- ["<C-e>"] = cmp.mapping.close(),
 
-    --[[ ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-e>"] = cmp.mapping.close(),
-    ["<c-y>"] = cmp.mapping.confirm {
+    --[[ ["<c-q>"] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Insert,
       select = true,
     },
     ["<c-q>"] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
-    }, ]]
+    },
 
-    ["<c-space>"] = cmp.mapping.complete(),
+    ["<c-space>"] = cmp.mapping.complete(), ]]
 
     -- If you want tab completion :'(
     --  First you have to just promise to read `:help ins-completion`.
@@ -45,44 +45,16 @@ cmp.setup {
 },
 
   sources = {
-    { name = "nvim_lua" },
-    { name = "zsh" },
-    { name = "luasnip" },
-
-
-    { name = "nvim_lsp" },
-    { name = "path" },
-    { name = "buffer", max_item_count = 4, keyword_length = 3 },
-  },
-
-  sorting = {
-    comparators = {
-      cmp.config.compare.offset,
-      cmp.config.compare.exact,
-      cmp.config.compare.score,
-
-      function(entry1, entry2)
-        local _, entry1_under = entry1.completion_item.label:find "^_+"
-        local _, entry2_under = entry2.completion_item.label:find "^_+"
-        entry1_under = entry1_under or 0
-        entry2_under = entry2_under or 0
-        if entry1_under > entry2_under then
-          return false
-        elseif entry1_under < entry2_under then
-          return true
-        end
-      end,
-
-      cmp.config.compare.kind,
-      cmp.config.compare.sort_text,
-      cmp.config.compare.length,
-      cmp.config.compare.order,
-    },
+    { name = "luasnip", max_item_count = 2 },
+    { name = "nvim_lua", max_item_count = 5 },
+    { name = "nvim_lsp", max_item_count = 3 },
+    { name = "path", max_item_count = 3 },
+    { name = "buffer", max_item_count = 2, keyword_length = 5 },
   },
 
   snippet = {
     expand = function(args)
-      require("luasnip").lsp_expand(args.body)
+      luasnip.lsp_expand(args.body)
     end,
   },
 
@@ -101,118 +73,7 @@ cmp.setup {
 
   experimental = {
     native_menu = false,
-    -- I've found it to be too annoying 
+    -- I've found it to be too annoying
     ghost_text = false,
   },
 }
-
---[[
-" Setup buffer configuration (nvim-lua source only enables in Lua filetype).
-"
-" ON YOUTUBE I SAID: This only _adds_ sources for a filetype, not removes the global ones.
-"
-" BUT I WAS WRONG! This will override the global setup. Sorry for any confusion.
-autocmd FileType lua lua require'cmp'.setup.buffer {
-\   sources = {
-\     { name = 'nvim_lua' },
-\     { name = 'buffer' },
-\   },
-\ }
---]]
-
--- Add vim-dadbod-completion in sql files
--- vim.cmd [[
---   augroup DadbodSql
---     au!
---     autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer { sources = { { name = 'vim-dadbod-completion' } } }
---   augroup END
--- ]]
-
---[[
-" Disable cmp for a buffer
-autocmd FileType TelescopePrompt lua require('cmp').setup.buffer { enabled = false }
---]]
-
--- Youtube: customizing appearance
---
--- nvim-cmp highlight groups.
---[[ local Group, groups = require("colorbuddy").setup()
-
-Group.new("CmpItemAbbr", groups.Comment)
-Group.new("CmpItemAbbrDeprecated", groups.Error)
-Group.new("CmpItemKind", groups.Special)
-Group.new("CmpItemMenu", groups.NonText) ]]
-
-
---[[ vim.o.completeopt = "menuone,noselect"
-
-require'compe'.setup {
-  enabled = true;
-  autocomplete = true;
-  debug = false;
-  min_length = 1;
-  preselect = 'enable';
-  throttle_time = 200;
-  source_timeout = 200;
-  resolve_timeout = 500;
-  incomplete_delay = 500;
-  max_abbr_width = 30;
-  max_kind_width = 30;
-  max_menu_width = 30;
-
-  documentation = true;
-
-  source = {
-    path = true;
-    buffer = true;
-    calc = true;
-    nvim_lsp = true;
-    nvim_lua = true;
-    vsnip = true;
-    ultisnips = true;
-    luasnip = true;
-    tags = true;
-    -- tabnine = {
-    --     max_num_results = 3;
-    --     show_prediction_strengh = false;
-    -- };
-  };
-}
-
-local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local check_back_space = function()
-    local col = vim.fn.col('.') - 1
-    if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        return true
-    else
-        return false
-    end
-end
-
--- Use (s-)tab to:
---- move to prev/next item in completion menuone
---- jump to prev/next snippet's placeholder
-_G.tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-n>"
-  elseif check_back_space() then
-    return t "<Tab>"
-  else
-    return vim.fn['compe#complete']()
-  end
-end
-_G.s_tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-p>"
-  else
-    return t "<S-Tab>"
-  end
-end
-
-vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true}) ]]
