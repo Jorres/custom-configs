57598e4dbfe7bf0e0731f1207e3ee1b4f310d190
Jorres Tarasov <jorres.tarasov@gmail.com>
2022-06-25 00:19:06 +0300

    feat: add initial configs for awesome

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,10 @@
 sudo apt-get update
-sudo apt-get install -y vim stow git gnome-tweaks zsh tmux ruby-full font-manager libusb-dev bat net-tools
+sudo apt-get install -y vim stow git gnome-tweaks zsh tmux ruby-full font-manager libusb-dev bat net-tools 
+
+# Stuff for awesome wm to work
+sudo apt-get install -y dmenu compton nitrogen alsa-utils xbacklight
+# Clone widget repository
+git clone https://github.com/streetturtle/awesome-wm-widgets.git ~/.config/awesome/awesome-wm-widgets
 
 # install brave
 
@@ -89,3 +94,18 @@ sudo apt install bashtop
 
 # A more sleek email client than Thunderbird
 sudo snap install mailspring
+
+# Install kitty
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+
+# The following chunk adds an icon of Kitty to desktop app menus. Works on Ubuntu!
+# Create a symbolic link to add kitty to PATH (assuming ~/.local/bin is in
+# your system-wide PATH)
+ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/
+# Place the kitty.desktop file somewhere it can be found by the OS
+cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+# If you want to open text files and images in kitty via your file manager also add the kitty-open.desktop file
+cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
+# Update the paths to the kitty and its icon in the kitty.desktop file(s)
+sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
