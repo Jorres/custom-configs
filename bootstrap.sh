67524c07d56ad68787612c5ccd1f9ccd1b6e4bc6
Jorres Tarasov <jorres.tarasov@gmail.com>
2024-05-10 00:14:39 +0200

    yak shaving

--- bootstrap.sh
+++ bootstrap.sh
@@ -209,3 +209,16 @@ python3 -m pip install libtmux --user
 
 # Flameshot
 sudo apt install flameshot
+
+# Install `gh` Github CLI
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+# Install `act` runner
+gh extension install https://github.com/nektos/gh-act
+# run it like:
+gh act -h
--- dot-config/.config/nvim/lua/plugins/chatgpt.lua
+++ dot-config/.config/nvim/lua/plugins/chatgpt.lua
@@ -5,7 +5,7 @@ require("gp").setup({
 		{
 			name = "ChatGPT4",
 			chat = true,
-			command = false,
+			command = true,
 			-- string with model name or table with model name and parameters
 			model = { model = "gpt-4-turbo", temperature = 0.8, top_p = 1 },
 			-- system prompt (use this to specify the persona/role of the AI)
@@ -16,11 +16,12 @@ require("gp").setup({
 					.. "- Don't elide any code from your output if the answer requires coding.\n"
 					.. "- Try to give your reply succintly. No need to paraphrase what you have already said.\n"
 					.. "- Assume the user is educated and smart, no need to explain minute details.\n"
-          .. "- For questions that can be answered with a single sentence or single example, you must do so without writing several hundred words"
+					.. "- For questions that can be answered with a single sentence or single example, you must do so without writing several hundred words"
 					.. "- Try to keep your explanations as short as possible. Whenever a question requires a simple reply, you must not additionally explain every step."
 					.. "- Take a deep breath; You've got this!\n",
 		},
-		{ name = "ChatGPT3.5", },
-		{ name = "CodeGPT3-5", },
+		{ name = "ChatGPT3-5" },
+		{ name = "CodeGPT4" },
+		{ name = "CodeGPT3-5" }
 	},
 })
