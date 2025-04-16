921152cd6de4ee0deb9b969f32ff7898a19e1190
Jorres Tarasov <jorres.tarasov@gmail.com>
2025-04-16 21:49:34 +0200

    feat: o3 o4 chatgpt

--- dot-config/.config/nvim/lua/plugins/chatgpt.lua
+++ dot-config/.config/nvim/lua/plugins/chatgpt.lua
@@ -1,5 +1,6 @@
 require("gp").setup({
 	chat_user_prefix = ":",
+	chat_template = require("gp.defaults").short_chat_template,
 	providers = {
  		deepseek = {
 			endpoint = "https://api.deepseek.com/v1/chat/completions",
@@ -57,36 +58,39 @@ require("gp").setup({
 	-- },
 	agents = {
 		{
-			name = "o1",
-			provider = "openai",
-			chat = true,
-			command = true,
-			model = { model = "o1" },
-			system_prompt = "You are a general very capable AI assistant.\n\n"
-		},
-		{
 			name = "ChatGPT4o",
-			chat = true,
-			command = true,
-			provider = "openai",
-			model = { model = "gpt-4o", temperature = 0.8, top_p = 1 },
-			system_prompt = "You are a general very capable AI assistant.\n\n"
-					.. "You are required to respond in the following way:\n\n"
-					.. "- If you're unsure don't guess and say you don't know instead.\n"
-					.. "- Ask questions if you need clarification to provide better answer.\n"
-					.. "- Don't elide any code from your output if the answer requires coding.\n"
-					.. "- Try to give your reply succintly. No need to paraphrase what you have already said.\n"
-					.. "- Assume the user is educated and smart, no need to explain minute details.\n"
-					.. "- For questions that can be answered with a single sentence or single example, you must do so without writing several hundred words"
-					.. "- Try to keep your explanations as short as possible. Whenever a question requires a simple reply, you must not additionally explain every step."
-					.. "- Take a deep breath; You've got this!\n",
+			disable = true,
 		},
 		{
 			name = "ChatGPT4o-mini",
+			disable = true,
+		},
+		{
+			name = "ChatGPT-o3-mini",
+			disable = true,
+		},
+		{
+			name = "o3-mini-high",
+			provider = "openai",
+			chat = true,
+			command = false,
+			model = { model = "o3-mini", reasoning_effort = "high" },
+			system_prompt = "You are a general very capable AI assistant.\n\n",
+		},
+		{
+			name = "o4-mini-high",
+			provider = "openai",
+			chat = true,
+			command = false,
+			model = { model = "o4-mini", reasoning_effort = "high" },
+			system_prompt = "You are a general very capable AI assistant.\n\n",
+		},
+		{
+			name = "gpt-4.1",
 			chat = true,
 			command = true,
 			provider = "openai",
-			model = { model = "gpt-4o-mini", temperature = 0.8, top_p = 1 },
+			model = { model = "gpt-4.1", temperature = 0.8, top_p = 1 },
 			system_prompt = "You are a general very capable AI assistant.\n\n"
 					.. "You are required to respond in the following way:\n\n"
 					.. "- If you're unsure don't guess and say you don't know instead.\n"
