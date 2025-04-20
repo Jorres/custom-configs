30ddb754aee7fd7a5f6a4bce19fee0b01422bcef
Jorres Tarasov <jorres.tarasov@gmail.com>
2025-04-20 18:30:34 +0200

    chore: move to blink-cmp, more visible floats

--- dot-config/.config/nvim/lua/plugins/autoc8n.lua
+++ dot-config/.config/nvim/lua/plugins/autoc8n.lua
@@ -1,111 +1,78 @@
-local lspkind = require "lspkind"
-lspkind.init()
+require "blink.cmp".setup({
 
-local cmp = require "cmp"
+  enabled = function() return not vim.tbl_contains({ "markdown" }, vim.bo.filetype) end,
+  -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+  -- 'super-tab' for mappings similar to vscode (tab to accept)
+  -- 'enter' for enter to accept
+  -- 'none' for no mappings
+  --
+  -- All presets have the following mappings:
+  -- C-space: Open menu or open docs if already open
+  -- C-n/C-p or Up/Down: Select next/previous item
+  -- C-e: Hide menu
+  -- C-k: Toggle signature help (if signature.enabled = true)
+  --
+  -- See :h blink-cmp-config-keymap for defining your own keymap
+  keymap = { preset = 'default' },
 
-cmp.setup {
-  completion = { completeopt = 'menu,menuone,noinsert' },
-
-  mapping = {
-    ["<c-y>"] = function(fallback)
-      if cmp.visible() then
-        cmp.confirm({
-          select = true,
-          behavior = cmp.ConfirmBehavior
-        })
-      else
-        fallback()
-      end
-    end,
-
-    -- I use moonlander keyboard, okay? I have my arrow keys where I want them
-    -- and they are super easy to reach. Regular keyboards users will
-    -- suffer. But <C-n> and <C-p> are also defined
-    -- for selecting options of the completion menu. Use them as well!
-    ["<Up>"] = cmp.mapping.select_prev_item(),
-    ["<Down>"] = cmp.mapping.select_next_item(),
-
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-n>"] = cmp.mapping.select_next_item(),
+  appearance = {
+    -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+    -- Adjusts spacing to ensure icons are aligned
+    nerd_font_variant = 'mono'
   },
 
+  -- (Default) Only show the documentation popup when manually triggered
+  completion = {
+    documentation = {
+      auto_show = false,
+    },
+
+    menu = {
+      border = "solid",
+      draw = {
+        treesitter = { 'lsp' },
+        columns = { { "label", "label_description", gap = 2, }, { "kind_icon", "kind", gap = 2 }, { "source_name" } },
+        components = {
+          kind_icon = {
+            text = function(ctx)
+              local lspkind = require("lspkind")
+              local icon = ctx.kind_icon
+              if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+                if dev_icon then
+                  icon = dev_icon
+                end
+              else
+                icon = lspkind.symbolic(ctx.kind, {
+                  mode = "symbol",
+                })
+              end
+
+              return icon .. ctx.icon_gap
+            end,
+          }
+        }
+      }
+    },
+  },
+
+  -- Default list of enabled providers defined so that you can extend it
+  -- elsewhere in your config, without redefining it, due to `opts_extend`
   sources = {
-    --     nvim_lsp = "[LSP]",
-    { name = "nvim_lsp_signature_help" },
-    { name = "nvim_lua",               max_item_count = 5, filetype = "lua" },
-    { name = "nvim_lsp",               keyword_length = 2, },
-    -- {
-    --   name = 'tmux',
-    --   option = {
-    --     all_panes = true,
-    --     label = '[tmux]',
-    --     trigger_characters = { '.' },
-    --     trigger_characters_ft = {} -- { filetype = { '.' } }
-    --   },
-    --   max_item_count = 3,
-    --   keyword_length = 3,
-    -- },
-    { name = "path",                   max_item_count = 3 },
-    { name = "buffer",                 max_item_count = 2, keyword_length = 4 },
+    default = { 'lsp', 'path', 'snippets', 'buffer' },
   },
 
-  formatting = {
-    format = lspkind.cmp_format {
-      mode = 'symbol',
-      maxwidth = 50,
-      symbol_map = {
-      },
-      --   menu = {
-      --     -- tmux = "[tmux]",
-      --     nvim_lsp_signature_help = "[sign]",
-      --     buffer = "[buf]",
-      --     nvim_lua = "[api]",
-      --     path = "[path]",
-      --   },
-    },
+  signature = {
+    enabled = true,
+    window = {
+      border = "solid",
+    }
   },
 
-  sorting = {
-    priority_weight = 2,
-    comparators = {
-      cmp.config.compare.offset,
-      cmp.config.compare.exact,
-      cmp.config.compare.score,
-      cmp.config.compare.recently_used,
-      cmp.config.compare.locality,
-      cmp.config.compare.kind,
-      cmp.config.compare.sort_text,
-      cmp.config.compare.length,
-      cmp.config.compare.order,
-    },
-  },
-
-  confirm_opts = {
-    behavior = cmp.ConfirmBehavior.Replace,
-    select = false,
-  },
-
-  experimental = {
-    native_menu = false,
-    ghost_text = false,
-  },
-}
-
-cmp.setup.filetype({ 'markdown' }, {
-  enabled = false,
-  mapping = {
-    ["<c-y>"] = cmp.mapping.confirm {
-      select = true,
-    },
-    ["<Up>"] = cmp.mapping.select_prev_item(),
-    ["<Down>"] = cmp.mapping.select_next_item(),
-  },
-  sources = {
-    { name = "path",   max_item_count = 3 },
-    { name = "buffer", max_item_count = 2, keyword_length = 4 },
-  },
-  experimental = {
-    native_menu = false,
-    ghost_text = true,
-  },
+  -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+  -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+  -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+  --
+  -- See the fuzzy documentation for more information
+  fuzzy = { implementation = "prefer_rust_with_warning" }
 })
--- dot-config/.config/nvim/lua/plugins/colorbuddy.lua
+++ dot-config/.config/nvim/lua/plugins/colorbuddy.lua
@@ -16,6 +16,14 @@ Color.new("myclr4", "#FFFFFF")
 Color.new("myclr5", "#393DC6")
 Color.new("myclr6", "#3DC639")
 Color.new("myclr7", "#404040")
+Color.new("completion", "#1e1c2a")
+Color.new("completion_selection", "#2f2b43")
+
+Color.new("icon1", "#393dc6")
+Color.new("icon2", "#c63983")
+Color.new("icon3", "#c6c239")
+Color.new("icon4", "#39c67c")
+
 Color.new("pinkBorder", "#E56399")
 
 Group.new("TelescopeBorder", colors.myclr1, colors.myclr1, nil)
@@ -31,6 +39,23 @@ Group.new("TelescopeSelection", nil, colors.myclr7, nil)
 Group.new("VibrantFloatBorder", colors.pinkBorder, nil, nil)
 Group.new("Directory", colors.pinkBorder, nil, nil)
 Group.new("NvimTreeFolderName", colors.pinkBorder, nil, nil)
+
+Group.new("Pmenu", nil, colors.completion, nil)
+Group.new("PmenuSel", nil, colors.completion_selection, nil)
+
+Group.new("BlinkCmpKindFunction", colors.icon1, nil, nil)
+Group.new("BlinkCmpKindText", colors.icon2, nil, nil)
+Group.new("BlinkCmpKindModule", colors.icon3, nil, nil)
+Group.new("BlinkCmpKindConstant", colors.icon4, nil, nil)
+Group.new("BlinkCmpKindVariable", colors.icon1, nil, nil)
+Group.new("BlinkCmpKindInterface", colors.icon2, nil, nil)
+Group.new("BlinkCmpKindMethod", colors.icon3, nil, nil)
+Group.new("BlinkCmpKindKeyword", colors.icon4, nil, nil)
+Group.new("BlinkCmpKindClass", colors.icon1, nil, nil)
+
+Group.new("NormalFloat", nil, colors.completion, nil)
+Group.new("NormalFloat", nil, colors.completion, nil)
+Group.new("FloatBorder", nil, colors.completion, nil)
 
 
 vim.cmd [[ highlight EndOfBuffer guifg=bg ]]
